b42ac24ff492e249cb1dcf31ace1942916aa05fb
lhiakrul <711hikaru@gmail.com>
2020-06-15 20:42:50 +0800

    html-webpack-plugin, clean-webpack-plugin

--- 2/src/index.js
+++ 2/src/index.js
@@ -1,9 +1,3 @@
-import avatar from './js.png'
-import './index.scss'
-
-var img = new Image()
-img.src = avatar
-img.classList.add('avatar')
-
 var root = document.getElementById('root')
-root.append(img)
+
+root.innerHTML = '<div class="test">abc</div>'
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 生成對應的 html
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -22,16 +24,24 @@ module.exports = {
                 test: /\.scss$/,
                 use: [
                     'style-loader', // 將唯一的 css 掛載至頁面 
-                    'css-loader', // 將多個 css 合併為一個 css
+                    {
+                        loader: 'css-loader', // 將多個 css 合併為一個 css
+                        options: {
+                            importLoaders: 2,
+                            modules: true
+                        }
+                    },
                     'sass-loader',
                     'postcss-loader' // web-kit, mozilla etc..
                 ]
             }
         ]
     },
+    plugins: [new HtmlWebpackPlugin({
+        template: '2/src/index.html'
+    }), new CleanWebpackPlugin()],
     output: {
-        filename: 'bundle.js',
-        path: 'bundle.js',
+        filename: 'test.js',
         path: path.resolve(__dirname, '2/dist')
     }
 }
